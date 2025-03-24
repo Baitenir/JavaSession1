5e6be8444907fdba73f73dcef6de7f3464051dbf
Baitenir <bakumake77@gmail.com>
2025-03-24 10:12:21 +0600

    Delete src/inheritence directory

--- src/inheritence/task4/Slack.java
+++
@@ -1,9 +0,0 @@
-package inheritence.task4;
-
-import java.time.LocalDate;
-
-public class Slack extends Chat{
-    public Slack (String name, boolean isNew, LocalDate dateOfIssue, String designColor){
-        super(name, isNew, dateOfIssue, designColor);
-    }
-}
